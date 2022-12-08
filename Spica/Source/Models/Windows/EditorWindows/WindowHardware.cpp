7f5ecbaedff581b9a912222893588d7d817a1d51
Gerard <gqueraltferre@gmail.com>
2022-12-08 18:52:50 +0100

    added libraries versions

--- Spica/Source/Models/Windows/EditorWindows/WindowHardware.cpp
+++ Spica/Source/Models/Windows/EditorWindows/WindowHardware.cpp
@@ -5,13 +5,13 @@
 #include "imgui.h"
 #include "glew.h"
 #include "SDL.h"
+#include "assimp/version.h"
 #include "d3d9.h"
 
 WindowHardware::WindowHardware() : EditorWindow("Hardware")
 {
 	m_flags |= ImGuiWindowFlags_AlwaysAutoResize;
 
-	GetSoftwareVersions();
 	GetCPUinfo();
 	GetCaps();
 	GetGPUinfo();
@@ -23,9 +23,15 @@ WindowHardware::~WindowHardware()
 
 void WindowHardware::DrawWindowContents()
 {
+	GetSoftwareVersions();
 
 	ImGui::TextUnformatted(("glew Version: " + m_glewVersion).c_str());
+	ImGui::TextUnformatted(("OpenGL Version: " + m_openGLVersion).c_str());
 	ImGui::TextUnformatted(("SDL Version: " + m_sdlVersion).c_str());
+	ImGui::TextUnformatted(("assimp Version: " + m_assimpVersion).c_str());
+	ImGui::TextUnformatted(("DirectXTex Version: " + m_dirextXTexVersion).c_str());
+	ImGui::TextUnformatted(("ImGui Version: " + m_imguiVersion).c_str());
+	ImGui::TextUnformatted(("MathGeoLib Version: " + m_mathGeoLibVersion).c_str());
 
 	ImGui::Separator();
 
@@ -43,11 +49,29 @@ void WindowHardware::DrawWindowContents()
 
 void WindowHardware::GetSoftwareVersions()
 {
-	char glewVersion[128];
-	sprintf(glewVersion, "%s", glewGetString(GLEW_VERSION));
-	m_glewVersion = std::string(glewVersion);
+	//this needs to be done after all modules have been initialized
+	//this way is a bit ugly but otherwise it would require a Start/Init function in Window that would be useless for the others
+	if (!m_versionsSet) {
+		m_versionsSet = true;
 
-	m_sdlVersion = std::to_string(SDL_MAJOR_VERSION) + "." + std::to_string(SDL_MINOR_VERSION) + "." + std::to_string(SDL_PATCHLEVEL);
+		char glewVersion[128];
+		sprintf(glewVersion, "%s", glewGetString(GLEW_VERSION));
+		m_glewVersion = std::string(glewVersion);
+
+		char glSupported[128];
+		sprintf(glSupported, "%s", glGetString(GL_VERSION));
+		m_openGLVersion = std::string(glSupported);
+
+		m_sdlVersion = FormatVersion(SDL_MAJOR_VERSION, SDL_MINOR_VERSION, SDL_PATCHLEVEL);
+
+		m_assimpVersion = FormatVersion(aiGetVersionMajor(), aiGetVersionMinor(), aiGetVersionRevision());
+
+		m_dirextXTexVersion = "1.9.7";
+
+		m_mathGeoLibVersion = "2.0";
+
+		m_imguiVersion = IMGUI_VERSION;
+	}
 }
 
 void WindowHardware::GetCPUinfo()
@@ -109,4 +133,9 @@ void WindowHardware::GetGPUinfo()
 	std::string gpuDevice = std::to_string(adapterIdentifier.DeviceId);
 	m_gpuVendorAndDevice = "vendor " + gpuVendor + " device " + gpuDevice;
 	m_gpuBrand = adapterIdentifier.Description;
+}
+
+std::string WindowHardware::FormatVersion(unsigned int i_major, unsigned int i_minor, unsigned int i_patch)
+{
+	return std::to_string(i_major) + "." + std::to_string(i_minor) + "." + std::to_string(i_patch);;
 }
--- Spica/Source/Models/Windows/EditorWindows/WindowHardware.h
+++ Spica/Source/Models/Windows/EditorWindows/WindowHardware.h
@@ -18,10 +18,20 @@ private:
 	void GetCaps();
 	void GetGPUinfo();
 
+	std::string FormatVersion(unsigned int i_major, unsigned int i_minor, unsigned int i_patch);
+
+	bool m_versionsSet = false;
 	std::string m_glewVersion;
+	std::string m_openGLVersion;
 	std::string m_sdlVersion;
+	std::string m_assimpVersion;
+	std::string m_dirextXTexVersion;
+	std::string m_imguiVersion;
+	std::string m_mathGeoLibVersion;
+
 	std::string m_cpusAndCache;
 	std::string m_caps;
+
 	std::string m_ram;
 	std::string m_gpuVendorAndDevice;
 	std::string m_gpuBrand;
