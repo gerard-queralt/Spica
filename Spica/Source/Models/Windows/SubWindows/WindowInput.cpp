1d02544dd6353b0a044fdabe5e38cf39c8d13b89
Gerard <gqueraltferre@gmail.com>
2022-12-08 18:36:44 +0100

    zoom speed modifiable

--- Spica/Source/Models/Windows/SubWindows/WindowInput.cpp
+++ Spica/Source/Models/Windows/SubWindows/WindowInput.cpp
@@ -19,4 +19,6 @@ void WindowInput::DrawWindowContents()
 	ImGui::SliderFloat("##movement", &App->input->m_cameraSpeed, 0.01f, 0.1f, "%.2f", ImGuiSliderFlags_AlwaysClamp);
 	ImGui::TextUnformatted("Rotation speed");
 	ImGui::SliderFloat("##rotation", &App->input->m_angleSpeed, 0.001f, 0.01f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+	ImGui::TextUnformatted("Zoom speed");
+	ImGui::SliderFloat("##zoom", &App->input->m_zoomSpeed, 0.001f, 0.01f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
 }
